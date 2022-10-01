08c7330d3c02c0a919c8c619b8ee6d8321850b2c
Caleb Moses <calebjdmoses@gmail.com>
2022-10-01 15:22:33 -0400

    Run linter

--- mais_hack/request_generation.py
+++ mais_hack/request_generation.py
@@ -4,27 +4,27 @@ from pyprojroot import here
 
 
 def get_api_key(secrets_yaml):
-    '''
+    """
     Returns the api key from a given secrets file
-    '''
-    with open(secrets_yaml, 'r') as f:
+    """
+    with open(secrets_yaml, "r") as f:
         secrets = yaml.load(f, Loader=yaml.FullLoader)
 
-    return secrets['api_key']
+    return secrets["api_key"]
 
 
 def request_generation(prompt, key=None, **kwargs):
 
-    '''
+    """
     Requests text generation via Co:here API
 
     Relevant here: https://docs.cohere.ai/generate-reference/
-    '''
+    """
 
     if key is None:
-        key = get_api_key(here('secrets.yaml'))
+        key = get_api_key(here("secrets.yaml"))
 
-    co = cohere.Client(f'{key}')
+    co = cohere.Client(f"{key}")
     response = co.generate(prompt=prompt, **kwargs)
 
     return response.generations[0].text
