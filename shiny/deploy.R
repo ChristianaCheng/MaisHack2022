4a82ba2d09a71f7df13525ffd365963530cff841
Caleb Moses <calebjdmoses@gmail.com>
2022-10-01 23:59:27 -0400

    Update deploy

--- shiny/deploy.R
+++ shiny/deploy.R
@@ -1,4 +1,15 @@
 library(here)
+library(yaml)
 library(rsconnect)
 
-rsconnect::deployApp(here('shiny'))
+get_shinyapps_credentials <- function(secrets_yaml) {
+  secrets = read_yaml(here(secrets_yaml))
+  return(secrets$shinyapps)
+}
+
+secrets <- get_shinyapps_credentials(here('secrets.yaml'))
+cat("Logging in...\n")
+do.call(rsconnect::setAccountInfo, secrets)
+
+cat("Deploying...\n")
+rsconnect::deployApp(appDir=here("shiny"), appName="helpium")
